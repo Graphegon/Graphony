95c742a15628292c19277b383e1f4c46012a77b9
Michel Pelletier <pelletier.michel@gmail.com>
2021-09-19 11:19:18 -0400

    long description in setup

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 import os
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="graphony",
     version="0.0.1",
     description="Graphony",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Michel Pelletier",
     packages=["graphony"],
     install_requires=[
